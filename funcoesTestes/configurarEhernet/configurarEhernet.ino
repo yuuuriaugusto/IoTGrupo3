04d1e92108c3a0399b0cfdb17e6e6653a631b461
Lubin Sebastien Lionel <lubinsebastienlionel@gmail.com>
2023-11-20 21:36:19 -0300

    configuraçao ethernet

--- funcoesTestes/configurarEhernet/configurarEhernet.ino
+++ funcoesTestes/configurarEhernet/configurarEhernet.ino
@@ -1,28 +1,50 @@
-#include <SPI.h>
-#include <Ethernet.h>
-
-IPAddress ip(192, 168, 1, 100); // Define o endereço IP desejado pode ser o seu
-EthernetServer server(80);      // Define a porta HTTP padrão
-
-void ConfigurarEthernet() {
-  byte mac[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05}; // Define o endereço MAC exclusivo
-
-  Serial.begin(9600); // Inicializa a porta serial para depuração
-
-  //  inicializar a Ethernet com o endereço MAC se nao deve retorna uma mensagem
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Erro na inicialização da Ethernet. Verifique a conexão e os cabos.");
-  } else {
-    server.begin(); // Inicializa o servidor na porta 80
-    Serial.print("Endereço IP: ");
-    Serial.println(Ethernet.localIP()); // Imprime o endereço IP na tela
-  }
-}
+#include <Arduino.h>
+#include <ETH.h>
+#include <WiFi.h>
 
 void setup() {
-  ConfigurarEthernet(); // Chamada da função de configuração da Ethernet no início do programa
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Iniciando...");
+
+  // Inicializa a Ethernet
+  if (ETH.begin() == 0) {
+    Serial.println("Erro na inicialização da Ethernet. Verifique a conexão e os cabos.");
+    while (1);
+  }
+
+  Serial.println("Ethernet conectada com sucesso!");
+
+  // Conecta-se à rede WiFi
+  Serial.print("Conectando-se à rede WiFi...");
+  if (WiFi.begin("Hyper Net Sebastien_Wi-Fi5", "Lubin175") == WL_CONNECTED) {
+    Serial.println(" Conexão WiFi bem-sucedida!");
+  } else {
+    Serial.println(" Falha na conexão WiFi!");
+    while (1);
+  }
+
+  // Mostra o endereço IP atribuído
+  Serial.print("Endereço IP atribuído: ");
+  Serial.println(WiFi.localIP());
 }
 
+void loop() {
+  // Endereço IP do Google
+  IPAddress googleServer(8, 8, 8, 8);
+
+  // Tenta fazer ping
+  Serial.print("Tentando fazer ping para google.com... ");
+  if (WiFi.hostByName("google.com", googleServer)) {
+    Serial.println("Ping bem-sucedido!");
+  } else {
+    Serial.println("Falha no ping!");
+  }
+
+  // Aguarda 5 segundos antes do próximo ping
+  delay(5000);
+}
 
 
 
